4ce33777a36ec8ca07633e84f5427386e9504b95
IsaGuzman55 <isabellaguzman143@gmail.com>
2024-03-22 20:38:09 -0500

    Creacion del controller de usuarios

--- Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a2b2736c50065e00ca63563610659390882a6cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
