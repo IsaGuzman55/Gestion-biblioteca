a2288a31574ef75facaf8e3fa576b2986abc81af
davidmachadoPY <avidmachado@gmail.com>
2024-03-26 17:59:09 -0500

    Primer commit: proyecto MVC

---
+++ Controllers/LibroController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc.Controllers{
+
+    public class LibroController : Controller{
+        public IActionResult Index(){
+            return View();
+        }
+
+        public IActionResult Crear(){
+            return View();
+        }
+        
+        public IActionResult Editar(){
+            return View();
+        }
+
+        public IActionResult Eliminar(){
+            return View();
+        }
+
+        public IActionResult Detalles(){
+            return View();
+        }
+    }
+}
